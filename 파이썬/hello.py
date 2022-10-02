cb5d8d0ec98b642db9973ceae62a01dbc3aceeed
우지안 <ujian@ujian-ui-MacBookAir.local>
2022-10-02 20:23:05 +0900

    First commit

---
+++ 파이썬/hello.py
@@ -0,0 +1,5 @@
+people = [{'name': 'bob', 'age': 20}, 
+          {'name': 'carry', 'age': 38},
+          {'name': 'john', 'age': 7},
+          {'name': 'smith', 'age': 17},
+          {'name': 'ben', 'age': 27}]
